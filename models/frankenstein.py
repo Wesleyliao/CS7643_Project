87795fef59bde20cb66310d6cff6c3c7a8d59c9a
Wesley Liao <wesleyliao3@gmail.com>
2021-04-15 00:52:08 -0400

    define frankenstein model

--- models/frankenstein.py
+++ models/frankenstein.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch.nn import functional as F
 
 
 class Frankenstein(nn.Module):
@@ -8,10 +9,34 @@ class Frankenstein(nn.Module):
         self.feature_model = feature_model
         self.generator = generator
 
-        self.fc1 = nn.Linear(512, 512, bias=False)
-        self.fc2 = nn.Linear(512, 512, bias=False)
+        self.fc1 = nn.Linear(512, 512)
+        self.fc2 = nn.Linear(512, 512)
 
     def forward(self, x):
 
+        x = F.interpolate(x, size=160)
+
         # x is (N, 3, 160, 160)
-        pass
+
+        face_features = self.feature_model(x)
+
+        # face_features is (N, 512)
+
+        x = self.fc1(face_features)
+        x = self.fc2(x)
+
+        # x is (N, 512)
+
+        anime_img = self.generator(x)
+
+        # x is (N, 3, 512, 512)
+
+        x = F.interpolate(anime_img, size=160)
+
+        # x is (N, 3, 160, 160)
+
+        anime_features = self.feature_model(x)
+
+        # anime_features is (N, 512)
+
+        return anime_img, face_features, anime_features
