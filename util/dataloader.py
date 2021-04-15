8b8196a0ead1ac1a8d4b76083a5485388f44e3a8
Wesley Liao <wesleyliao3@gmail.com>
2021-04-15 00:38:55 -0400

    add resize option to data loader

--- util/dataloader.py
+++ util/dataloader.py
@@ -7,17 +7,26 @@ from torchvision import transforms
 log = logging.getLogger(__name__)
 
 
-def get_dataloader(path: str, batch_size: int = 32) -> DataLoader:
+def get_dataloader(
+    path: str, batch_size: int = 32, random_flip: bool = False, image_size: int = None
+) -> DataLoader:
+
+    # Define transformations
+    transforms_list = []
+
+    if random_flip:
+        transforms_list.append(transforms.RandomHorizontalFlip())
+
+    if image_size:
+        transforms_list.append(transforms.Resize(image_size))
+
+    transforms_list.append(transforms.ToTensor())
+    transforms_list.append(
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    )
 
     # Transform train data with augmentation
-    transform = transforms.Compose(
-        [
-            # transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ]
-    )
+    transform = transforms.Compose(transforms_list)
 
     # Get dataset
     dataset = datasets.ImageFolder(path, transform=transform)
