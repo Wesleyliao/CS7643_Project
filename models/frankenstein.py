3085d04a676ccac62f73ea0b3a52622ebd2c8cba
Wesley Liao <wesleyliao3@gmail.com>
2021-04-15 22:19:53 -0400

    I think it's working

--- models/frankenstein.py
+++ models/frankenstein.py
@@ -9,20 +9,27 @@ class Frankenstein(nn.Module):
         self.feature_model = feature_model
         self.generator = generator
 
+        for params in self.feature_model.parameters():
+            params.requires_grad = False
+
+        for params in self.generator.parameters():
+            params.requires_grad = False
+
         self.fc1 = nn.Linear(512, 512)
         self.fc2 = nn.Linear(512, 512)
 
     def forward(self, x):
 
         x = F.interpolate(x, size=160)
-
         # x is (N, 3, 160, 160)
 
         face_features = self.feature_model(x)
 
         # face_features is (N, 512)
 
-        x = self.fc1(face_features)
+        x = F.relu(face_features)
+        x = self.fc1(x)
+        x = F.relu(x)
         x = self.fc2(x)
 
         # x is (N, 512)
@@ -38,5 +45,5 @@ class Frankenstein(nn.Module):
         anime_features = self.feature_model(x)
 
         # anime_features is (N, 512)
-
+        # print(f'outputting', anime_img.size(), face_features.size(), anime_features.size())
         return anime_img, face_features, anime_features
--- pretrain_stylegan_adapter.py
+++ pretrain_stylegan_adapter.py
@@ -4,8 +4,10 @@ import sys
 import torch
 import yaml
 
+from models.frankenstein import Frankenstein
 from pretrain import resnet_face_model
 from pretrain import stylegan2_model
+from util.dataloader import get_dataloader
 
 # Logging
 log = logging.getLogger(__name__)
@@ -21,6 +23,25 @@ logging.basicConfig(
 CONFIG_PATH = './config/pretrain_stylegan_adapter.yml'
 with open(CONFIG_PATH, 'r') as stream:
     CONFIG = yaml.safe_load(stream)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
 def get_pretrained_stylegan(path):
@@ -41,13 +62,71 @@ def get_pretrained_resnet(path):
     return model
 
 
+def train(epoch, data_loader, model, optimizer):
+
+    losses = AverageMeter()
+
+    for idx, (data, _) in enumerate(data_loader):
+
+        if torch.cuda.is_available():
+            data = data.cuda()
+
+        optimizer.zero_grad()
+
+        # Forward pass
+        anime_img, face_features, anime_features = model(data)
+
+        criterion = torch.nn.MSELoss()
+        loss = criterion(face_features, anime_features)
+
+        # Backward pass
+        loss.backward()
+        optimizer.step()
+
+        # Record loss
+        losses.update(loss, anime_img.shape[0])
+
+    log.info(f'Epoch {epoch} loss {losses.avg}')
+
+
 def main():
 
+    # Get portrait dataloader
+    ffhq_loader = get_dataloader(CONFIG['ffhq_path'], CONFIG['batch_size'])
+
+    # Get pretrained StyleGAN2 generator
     stylegan_model = get_pretrained_stylegan(CONFIG['pretrain_stylgan_path'])
     print(stylegan_model(torch.randn(2, 512)).size())
 
+    # Get face feature extractor
     resnet_model = get_pretrained_resnet(CONFIG['pretrain_resnet_path'])
     print(resnet_model(torch.randn(2, 3, 160, 160)).size())
+
+    # Get main model
+    model = Frankenstein(resnet_model, stylegan_model)
+
+    # Move model to cuda if it is available
+    if torch.cuda.is_available():
+        model = model.cuda()
+
+    # Log trainable layers
+    for param in model.parameters():
+        if param.requires_grad:
+            log.info(f'Trainable layer: {param.size()} {param.device}')
+
+    # Setup
+    best_model = None
+    float('inf')
+    optimizer = torch.optim.Adam(model.parameters(), CONFIG['lr'])
+
+    # Train
+    log.info('Starting training...')
+    for i in range(10):
+
+        train(i, ffhq_loader, model, optimizer)
+
+    # Save model
+    torch.save(best_model.state_dict(), './checkpoints/pretrained_adapter.pth')
 
 
 if __name__ == '__main__':
