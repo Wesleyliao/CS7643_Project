a2fa6bb856f489dba000e194f9993dc44ebbbf89
Gary Liu <gliu@DRWHoldings.com>
2021-04-27 15:40:15 -0400

    add edge smooth

--- animeGAN/util/edge_smooth.py
+++ animeGAN/util/edge_smooth.py
@@ -1,5 +1,4 @@
 # The edge_smooth.py is from taki0112/CartoonGAN-Tensorflow https://github.com/taki0112/CartoonGAN-Tensorflow#2-do-edge_smooth
-from utils import check_folder
 import numpy as np
 import cv2, os, argparse
 from glob import glob
@@ -8,23 +7,23 @@ from tqdm import tqdm
 def parse_args():
     desc = "Edge smoothed"
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('--dataset', type=str, default='Paprika', help='dataset_name')
+    parser.add_argument('--src-dir', type=str, default='data/ffhq2anime/trainA', help='dataset_dir')
+    parser.add_argument('--tgt-dir', type=str, default='data/ffhq2anime/trainA/smooth', help='dataset_dir')
     parser.add_argument('--img_size', type=int, default=256, help='The size of image')
 
     return parser.parse_args()
 
-def make_edge_smooth(dataset_name, img_size) :
-    check_folder('./dataset/{}/{}'.format(dataset_name, 'smooth'))
-
-    file_list = glob('./dataset/{}/{}/*.*'.format(dataset_name, 'style'))
-    save_dir = './dataset/{}/smooth'.format(dataset_name)
+def make_edge_smooth(src_dir, tgt_dir, img_size) :
+    
+    file_list = glob(f'{src_dir}/*.*')
+    save_dir = f'{tgt_dir}'
 
     kernel_size = 5
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
     gauss = cv2.getGaussianKernel(kernel_size, 0)
     gauss = gauss * gauss.transpose(1, 0)
 
-    for f in tqdm(file_list) :
+    for f in tqdm(file_list):
         file_name = os.path.basename(f)
 
         bgr_img = cv2.imread(f)
@@ -56,7 +55,7 @@ def main():
     if args is None:
         exit()
 
-    make_edge_smooth(args.dataset, args.img_size)
+    make_edge_smooth(args.src_dir, args.tgt_dir, args.img_size)
 
 
 if __name__ == '__main__':
